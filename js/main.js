37253e0b8d2612b8dcf453b706534dbcbf7c9ade
Selene Clark <michellc@byui.edu>
2022-01-08 14:09:19 -0700

    Week 1 notes. Console log showing up in the browser

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 
-            const links = [
-                {
-                    label: "Week 1",
-                    url: "week1/index.html"
-                },
-                {
-                    label: "Week 2",
-                    url: "week2/index.html"
-                }
-            ]
-            
-            function createList(){
-                for (var i=0; i < links.length; i++){
-                    var listOfWeeks = document.getElementById("myList").innerHTML +=
-                        '<li>' + '<a href="'+links[i].url+'">'+links[i].label+'</a>'+'</li>'
+const links = [
+    {
+        label: "Week 1",
+        url: "week1/index.html"
+    },
+    {
+        label: "Week 2",
+        url: "week2/index.html"
+    }
+]
 
-                }
-            }
+function createList(){
+    console.log("Console.log = Hello. This has been logged to the console")
+    for (var i=0; i < links.length; i++){
+        var listOfWeeks = document.getElementById("myList").innerHTML +=
+            '<li>' + '<a href="'+links[i].url+'">'+links[i].label+'</a>'+'</li>'
+
+    }
+}
             
         
